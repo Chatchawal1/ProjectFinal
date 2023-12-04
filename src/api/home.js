2645f383540d5c726fe3edf61245c7f59e5173bc
MSI\pow <chatchawalwas@gosoft.co.th>
2023-12-05 04:26:13 +0700

    new2

--- src/api/home.js
+++ src/api/home.js
@@ -1,21 +1,48 @@
-import React, { useEffect, useState } from "react";
+// home.js
+const express = require("express");
+const db = require("../database/db"); // ตรวจสอบ path ตามโครงสร้างของโปรเจค
 
-const SportList = () => {
-  const [sports, setSports] = useState([]);
+const router = express.Router();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("http://localhost:3000/api/sport");
-        const data = await response.json();
-        setSports(data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
+router.get("/equipmentCount", (req, res) => {
+  const queryRecreational =
+    "SELECT SUM(amount) as totalRecreational FROM recreational";
+  const querySport = "SELECT SUM(amount) as totalSport FROM sport";
+
+  db.query(queryRecreational, (errorRecreational, resultsRecreational) => {
+    if (errorRecreational) {
+      console.error("Error querying recreational table:", errorRecreational);
+      res.status(500).json({ error: "Internal Server Error" });
+      return;
+    }
+
+    db.query(querySport, (errorSport, resultsSport) => {
+      if (errorSport) {
+        console.error("Error querying sport table:", errorSport);
+        res.status(500).json({ error: "Internal Server Error" });
+        return;
       }
-    };
 
-    fetchData();
-  }, []);
-};
+      const totalRecreational = resultsRecreational[0].totalRecreational || 0;
+      const totalSport = resultsSport[0].totalSport || 0;
+      const total = totalRecreational + totalSport;
 
-export default SportList;
+      res.json({ totalRecreational, totalSport, total });
+    });
+  });
+});
+router.get("/userCount", (req, res) => {
+  const query = "SELECT COUNT(*) AS userCount FROM user";
+
+  db.query(query, (error, results) => {
+    if (error) {
+      console.error("Error querying database:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+      return;
+    }
+
+    const userCount = results[0].userCount;
+    res.json({ userCount });
+  });
+}); //จำนวนผู้ใช้ทั้งหมด
+module.exports = router;
